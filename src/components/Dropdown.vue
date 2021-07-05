ff059811b32adfe04f1cefc3d16aae28442af03a
Michael <m.tracey5021@gmail.com>
2021-07-05 16:44:40 +1200

    cleanup

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -1,30 +1,13 @@
 <template>
 
-    <!-- <div class="dropdown-container">
-        <button class="btn" v-on:click="toggleDropdown">
-            Dropdown
-        </button>
-        
-        <ul v-if="displayDropdownData" class="dropdown">
-            <DropdownItem v-for="(item, index) in itemData" :key="item" :menuItemId="'' + index" :label="item.label" :icon="item.icon" :command="item.command" :children="item.children"/>
-        </ul>
-    </div> -->
-
     <div class="dropdown-container">
         <button class="btn" v-on:click="toggleDropdown">
             {{ textData }}
         </button>
-        <!-- <div id="dropdown-wrapper" class="collapsed relative">
-            <ul id="dropdown" class="dropdown">
-                <DropdownItem v-for="(item, index) in itemData" :key="item" :menuItemId="'' + index" :label="item.label" :icon="item.icon" :command="item.command" :children="item.children"/>
-            </ul>
-        </div> -->
         <ul v-if="displayDropdownData" class="dropdown">
             <DropdownItem v-for="(item, index) in itemData" :key="item" :menuItemId="'' + index" :label="item.label" :icon="item.icon" :command="item.command" :children="item.children"/>
         </ul>
     </div>
-
-
 
 </template>
 
@@ -59,77 +42,11 @@ export default {
         toggleDropdown: function(){
             this.displayDropdownData = !this.displayDropdownData;
         },
-        expandDropdown: function(){
-            // debugger;
-            var dropdownWrapper = document.getElementById('dropdown-wrapper');
-            if (dropdownWrapper.clientHeight){
-                dropdownWrapper.style.height = 0;
-            }else{
-                var dropdown = document.getElementById('dropdown');
-                var expandedHeight = dropdown.clientHeight + 15;
-                dropdownWrapper.style.height = expandedHeight + 'px';
-            }
-        }
     }
 }
 
 </script>
 
 <style>
-
-/* .mt-dropdown-container {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    z-index: 10;
-    
-
-}
-
-.mt-dropdown-container .mt-dropdown {
-    list-style-type: none;
-    margin-top: 4px;
-    padding: 0;
-    border: 1px solid rgba(106, 106, 106, 0.2);
-    border-radius: 4px;
-    box-shadow: rgba(0, 0, 0, 0.15) 0px 5px 15px 0px;
-} */
-
-.mt-dropdown-container {
-    position: relative;
-    
-
-}
-
-.mt-dropdown-container .mt-dropdown {
-    position: absolute;
-    list-style-type: none;
-    margin-top: 4px;
-    padding: 0;
-    border: 1px solid rgba(106, 106, 106, 0.2);
-    border-radius: 4px;
-    box-shadow: rgba(0, 0, 0, 0.15) 0px 5px 15px 0px;
-    /* z-index: 10; */
-}
-
-/* .overlay-escape {
-    opacity: 0;
-    background: #000; 
-    width:      100%;
-    height:     100%; 
-    z-index:    -1;
-    top:        0; 
-    left:       0; 
-    position:   fixed; 
-} */
-
-.collapsed {
-    -moz-transition: height .25s;
-    -webkit-transition: height .25s;
-    transition: height .25s;
-    height: 0;
-    overflow: hidden;
-}
-
 
 </style>
